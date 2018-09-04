2bc91bbb38f9f8c806142d913ce90bde91308d38
kevinl94303 <kevinl94303spam@gmail.com>
2018-09-04 00:29:06 -0400

    added navbar and changed featured projects

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const StyledNavbar = styled.div`
+    height: 50px;
+    width: 100vw;
+    background: linear-gradient(black, #333);
+    position: fixed;
+    top: 0;
+    z-index: 10;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+const NavbarItem = styled.div`
+    text-align: center;
+    position: relative;
+
+    &:hover{
+        text-decoration: none;
+        color: white;
+    }
+    &:before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: -3px;
+        left: 0;
+        background-color: white;
+        visibility: hidden;
+        transform: scaleX(0);
+        transform-origin: left;
+        transition: all .1s cubic-bezier(1,.25,0,.75) 0s;;
+    }
+    &:hover:before {
+        visibility: visible;
+        transform: scaleX(1);
+    }
+`
+
+class Navbar extends Component{
+    render() {
+        return(
+            <StyledNavbar>
+                <NavbarItem>Top</NavbarItem>
+                <NavbarItem>Featured</NavbarItem>
+                <NavbarItem>2018</NavbarItem>
+                <NavbarItem>2017</NavbarItem>
+            </StyledNavbar>
+        )
+    }
+}
+
 const StyledFeature = styled.div`
     grid-column: span 4;
-    height: 20vw;
+    height: 30vw;
     overflow: hidden;
     position: relative;
     cursor: pointer;
@@ -22,7 +74,21 @@ const FeatureBackground = styled.div`
     }
 `
 
-const FeatureTitle = styled.div`
+const VICEFeatureBackground = styled(FeatureBackground)`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.9)),url("${props => props.item.background}");
+    background-size: cover;
+    background-blend-mode: multiply;
+    background-position: center bottom;
+    &:hover{
+        filter: blur(5px);
+        transform: scale(1.07);
+    }
+`
+
+const FeatureName = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
@@ -41,7 +107,18 @@ class Feature extends Component{
         return(
             <StyledFeature>
                 <FeatureBackground item={this.props.item}/>
-                <FeatureTitle>{this.props.item.name}</FeatureTitle>
+                <FeatureName>{this.props.item.name}</FeatureName>
+            </StyledFeature>
+        )
+    }
+}
+
+class VICEFeature extends Component{
+    render() {
+        return(
+            <StyledFeature>
+                <VICEFeatureBackground item={this.props.item}/>
+                <FeatureName>{this.props.item.name}</FeatureName>
             </StyledFeature>
         )
     }
@@ -49,7 +126,6 @@ class Feature extends Component{
 
 const ContentContainer = styled.div`
     width: 100vw;
-    height: 100vh;
 `
 const Content = styled.div`
     display: grid;
@@ -59,7 +135,7 @@ const Content = styled.div`
 
 const Intro = styled.div`
     grid-column: 2/5;
-    height: 20vw;
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -69,6 +145,9 @@ const Intro = styled.div`
 
 const IntroBlurb = styled.div`
     grid-column: 6/13;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     font-size: 2rem;
     color: #888;
     & a{
@@ -81,7 +160,6 @@ const IntroBlurb = styled.div`
 
 const FeaturedTitle = styled.div`
     grid-column: 1/13;
-    margin-top: 30px;
     height: 20vw;
     display: flex;
     align-items: center;
@@ -99,7 +177,7 @@ const nsop2018 = {
 
 const vicePodcast = {
     name: "VICE Podcast Revamp Project",
-    background: ""
+    background: "/mobile-player.gif"
 }
 
 const yearInReview = {
@@ -110,28 +188,33 @@ const yearInReview = {
 class PageContent extends Component {
     render(){
         return(
-            <ContentContainer>
-                <Content>
-                    <Intro>
-                    About me
-                    </Intro>
-                    <IntroBlurb>
-                    I'm a computer science student at Columbia University with a passion for web products and AI/Machine Learning.
-                    <br/>
-                    <br/>
-                    Here's my <a href="" target="_blank">resume</a>, <a href="" target="_blank">LinkedIn profile</a>, and <a href="" target="_blank">Github</a>.
-                    <br/>
-                    <br/>
-                    See some of my past projects below.
-                    </IntroBlurb>
-                    <FeaturedTitle>
-                    Featured Projects
-                    </FeaturedTitle>
-                    <Feature item={nsop2018}></Feature>
-                    <Feature item={vicePodcast}></Feature>
-                    <Feature item={yearInReview}></Feature>
-                </Content>
-            </ContentContainer>
+            <div>
+                <Navbar/>
+                <ContentContainer>
+                    <Content>
+                        <Intro>
+                        About me
+                        </Intro>
+                        <IntroBlurb>
+                            <div>
+                                I'm a computer science student at Columbia University with a passion for web products and AI/Machine Learning.
+                                <br/>
+                                <br/>
+                                Here's my <a href="" target="_blank">resume</a>, <a href="" target="_blank">LinkedIn profile</a>, and <a href="" target="_blank">Github</a>.
+                                <br/>
+                                <br/>
+                                Here's some of the stuff I've worked on.
+                            </div>
+                        </IntroBlurb>
+                        <FeaturedTitle>
+                        Featured Projects
+                        </FeaturedTitle>
+                        <Feature item={nsop2018}></Feature>
+                        <VICEFeature item={vicePodcast}></VICEFeature>
+                        <Feature item={yearInReview}></Feature>
+                    </Content>
+                </ContentContainer>
+            </div>
         )
     }
 }
